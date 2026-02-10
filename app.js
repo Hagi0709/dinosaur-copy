71358d672eed625a6926c8a0670eb46da936f5e8
Hagi0709 <67847736+Hagi0709@users.noreply.github.com>
2026-02-11 07:36:33 +0900

    app.js を更新

--- app.js
+++ app.js
@@ -2144,6 +2144,17 @@ let entryPw = loadJSON(LS.ROOM_ENTRY_PW, '2580');
     ROOM8: '0000',
     ROOM9: '0000',
   });
+  let roomUser = loadJSON(LS.ROOM_USER, {
+    ROOM1: '',
+    ROOM2: '',
+    ROOM3: '',
+    ROOM4: '',
+    ROOM5: '',
+    ROOM6: '',
+    ROOM7: '',
+    ROOM8: '',
+    ROOM9: '',
+  });
 
   async function copyText(text) {
     try {
